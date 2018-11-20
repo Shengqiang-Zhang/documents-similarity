b43926a46d3d476dfe0ab834d09f0e4c70e05471
zhangsq <zhangsq5829@gmail.com>
2018-11-20 19:44:41 +0800

    change the method of counting document nums.

--- dictionary_builder.py
+++ dictionary_builder.py
@@ -12,21 +12,24 @@ class DictionaryBuilder:
         word_dict = dict()
         doc_list = []
         with open(self.datafile, "r", encoding="utf-8") as f:
+            doc_id_list = []
+            doc = []
             for line in f.readlines():
-                line = line.strip().split("  ")[1:]
+                line = line.strip().split("  ")[0:]
                 if len(line) == 0:
                     continue
-                doc = []
-                for word_pos in line:
+                if line[0][:15] not in doc_id_list:
+                    doc_id_list.append(line[0][:15])
+                    if len(doc) > 0:
+                        doc_list.append(doc)
+                    doc = []
+                for word_pos in line[1:]:
                     word = word_pos.split("/")[0]
                     pos = word_pos.split("/")[1]
                     if pos == "w":
                         continue
                     doc.append(word)
                     word_dict[word] = word_dict.get(word, 0) + 1
-                if len(doc) == 0:
-                    continue
-                doc_list.append(doc)
         return word_dict, doc_list
 
     def max_word_frequency(self):
@@ -39,7 +42,8 @@ if __name__ == '__main__':
     db = DictionaryBuilder("data/199801_clear_1.txt")
     # test build_dictionary
     word_dict, doc_list = db.build_dictionary()
-
     print(len(doc_list))
+
+    # print(len(doc_list))
     # print(doc_list[:5])
     # print(db.max_word_frequency())
