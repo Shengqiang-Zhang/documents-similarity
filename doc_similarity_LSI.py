ac7e161f468062f57ff2b47fd53071c422a4a091
zhangsq <zhangsq5829@gmail.com>
2018-11-21 15:16:08 +0800

    finish LSI model

--- doc_similarity_LSI.py
+++ doc_similarity_LSI.py
@@ -4,6 +4,7 @@ warnings.filterwarnings(action="ignore", category=UserWarning, module="gensim")
 
 from gensim import corpora, similarities, models
 from dictionary_builder import DictionaryBuilder
+import numpy as np
 
 DOCUMENT_FILE = "data/199801_clear_1.txt"
 DOCUMENT_FILE_TEST = "data/small_data_for_test.txt"  # small datafile for testing
@@ -11,7 +12,6 @@ GENSIM_TEST_FILE = "data/gensim_testfile.txt"  # small file for testing gensim
 
 
 class LSIModel:
-
     def __init__(self, datafile: str):
         self._word_dict, self._doc_list = DictionaryBuilder(datafile).build_dictionary()
         self._dictionary = corpora.Dictionary(self._doc_list)
@@ -30,47 +30,47 @@ class LSIModel:
         _doc_tf_idf_vec = tf_idf_model[self._doc_bow_vec]
         return _doc_tf_idf_vec
 
-    def similarity_vec(self):
-        similarity = similarities.Similarity("Similarity-tfidf-index",
-                                             corpus=self._doc_tf_idf_vec,
-                                             num_features=len(self._dictionary))
-        for idx, sim in enumerate(similarity):
-            print(idx, sim)
-        return similarity
+    def similarity_vec_tf_idf(self):
+        similarity_vec_tf_idf = similarities.Similarity("Similarity-tfidf-index",
+                                                        corpus=self._doc_tf_idf_vec,
+                                                        num_features=len(self._dictionary))
+        return similarity_vec_tf_idf
 
-    def test_similarity(self):
-        test_data = ["十五大", "会议", "精神"]
-        test_bow_vec = self._dictionary.doc2bow(test_data)
-        similarity_vec = self.similarity_vec()
-        similarity_vec.num_best = 2
-        tf_idf_model = models.TfidfModel(self._doc_bow_vec)
-        test_tf_idf_vec = tf_idf_model[test_bow_vec]
-        print(test_tf_idf_vec)
-        print(similarity_vec[test_tf_idf_vec])
-        # print(similarity_vec)
-
-    def lsi_model(self):
+    def similarity_vec_lsi(self):
         lsi = models.LsiModel(self._doc_tf_idf_vec)
         doc_lsi = lsi[self._doc_tf_idf_vec]
-        similarity_vec = similarities.Similarity("Similarity-LSI-index",
-                                                 corpus=doc_lsi,
-                                                 num_features=len(self._dictionary))
-        for idx, sim in enumerate(similarity_vec):
-            print(idx, sim)
+        similarity_vec_lsi = similarities.Similarity("Similarity-LSI-index",
+                                                     corpus=doc_lsi,
+                                                     num_features=len(self._dictionary))
+        for i in doc_lsi:
+            print(i)
+        for sim in similarity_vec_lsi:
+            print(sim)
+        return similarity_vec_lsi
+
+    def save_vec_to_file(self, filename: str):
+        similarity_vec = self.similarity_vec_lsi()
+        vec = []
+        for sim in similarity_vec:
+            vec.append(sim)
+        numpy_vec = np.array(vec)
+        np.savetxt(filename, numpy_vec)
+
 
 
 if __name__ == '__main__':
-    lsi_model = LSIModel(DOCUMENT_FILE_TEST)
+    lsi_model = LSIModel(DOCUMENT_FILE)
     lsi_model.print_doc_attr()
     # print(lsi_model._doc_list)
     # print(lsi_model._dictionary)
 
-    # test similarity_vec
+    # test similarity_vec_tf_idf
     # similarity_vec = lsi_model.similarity_vec()
     # print(similarity_vec)
 
-    # test lsi_model
-    # lsi_model.lsi_model()
+    # test similarity_vec_lsi
+    # doc_similarity_vec = lsi_model.similarity_vec_lsi()
+    # print(doc_similarity_vec)
 
-    # test test_similarity
-    lsi_model.test_similarity()
+    # test save_model_to_file
+    lsi_model.save_vec_to_file("similarity_vec_lsi.txt")
