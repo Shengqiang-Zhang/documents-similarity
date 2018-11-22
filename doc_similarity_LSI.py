0e5c6fd8ab55f2527f7b393e758219a84e34a786
zhangsq <zhangsq5829@gmail.com>
2018-11-22 20:03:59 +0800

    finish LSI model.

--- doc_similarity_LSI.py
+++ doc_similarity_LSI.py
@@ -11,7 +11,7 @@ DOCUMENT_FILE_TEST = "data/small_data_for_test.txt"  # small datafile for testin
 GENSIM_TEST_FILE = "data/gensim_testfile.txt"  # small file for testing gensim
 
 
-class LSIModel:
+class DocSimilarityLSI:
     def __init__(self, datafile: str):
         self._word_dict, self._doc_list = DictionaryBuilder(datafile).build_dictionary()
         self._dictionary = corpora.Dictionary(self._doc_list)
@@ -42,35 +42,36 @@ class LSIModel:
         similarity_vec_lsi = similarities.Similarity("Similarity-LSI-index",
                                                      corpus=doc_lsi,
                                                      num_features=len(self._dictionary))
-        for i in doc_lsi:
-            print(i)
-        for sim in similarity_vec_lsi:
-            print(sim)
-        return similarity_vec_lsi
+        return lsi, similarity_vec_lsi
 
-    def save_vec_to_file(self, filename: str):
-        similarity_vec = self.similarity_vec_lsi()
-        vec = []
-        for sim in similarity_vec:
-            vec.append(sim)
-        numpy_vec = np.array(vec)
-        np.savetxt(filename, numpy_vec)
+    def save_model_to_file(self, lsi_model, similarity_model, lsi_file: str, sim_file: str):
+        lsi_model.save(lsi_file)
+        similarity_model.save(sim_file)
 
+    def analyze_result_model(self, doc_id: int, *lsi_model_file, similarity_file, load_model_from_file=False):
+        if load_model_from_file:
+            lsi_model = models.LsiModel.load(*lsi_model_file)
+            similarity_vec_lsi = similarities.Similarity.load(similarity_file)
+        else:
+            lsi_model = models.LsiModel(self._doc_tf_idf_vec)
+            doc_lsi = lsi_model[self._doc_tf_idf_vec]
+            similarity_vec_lsi = similarities.Similarity("Similarity-LSI-index",
+                                                         corpus=doc_lsi,
+                                                         num_features=len(self._dictionary))
+        doc_lsi_vec = lsi_model[self._doc_tf_idf_vec[doc_id]]
+        doc_sim_list = similarity_vec_lsi[doc_lsi_vec]
+        sim_list = np.argsort(-doc_sim_list)
+        print("the base doc:")
+        print(self._doc_list[doc_id])
+        print("three mostly similar doc:")
+        for idx, i in enumerate(sim_list[:4]):
+            print(idx, self._doc_list[i])
 
 
 if __name__ == '__main__':
-    lsi_model = LSIModel(DOCUMENT_FILE)
-    lsi_model.print_doc_attr()
-    # print(lsi_model._doc_list)
-    # print(lsi_model._dictionary)
+    doc_sim = DocSimilarityLSI(DOCUMENT_FILE)
+    # doc_sim.print_doc_attr()
 
-    # test similarity_vec_tf_idf
-    # similarity_vec = lsi_model.similarity_vec()
-    # print(similarity_vec)
-
-    # test similarity_vec_lsi
-    # doc_similarity_vec = lsi_model.similarity_vec_lsi()
-    # print(doc_similarity_vec)
-
-    # test save_model_to_file
-    lsi_model.save_vec_to_file("similarity_vec_lsi.txt")
+    # lsi_model, doc_similarity_vec = doc_sim.similarity_vec_lsi()
+    # doc_sim.save_model_to_file(lsi_model, doc_similarity_vec, "lsi.model", "sim.model")
+    doc_sim.analyze_result_model(29, "lsi.model", similarity_file="sim.model", load_model_from_file=True)
